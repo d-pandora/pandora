d471f66fcaf3b40f25751b9be01e08f5bf760f55
chenjunrui <chenjunrui@dianwoba.com>
2019-10-18 11:45:02 +0800

    react lazy & error boundary

--- templates/react+express+typescript/project/src/view/routes.tsx
+++ templates/react+express+typescript/project/src/view/routes.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { Route } from 'react-router-dom'
 import Layout from 'pages/layout'
+import ErrorBoundary from './errorBoundary'
 
-import about from 'pages/about'
-import userList from 'pages/user/list/index'
+const About = lazy(() => import('pages/about'))
+const UserList = lazy(() => import('pages/user/list/index'))
 
 const routes = (
   <Layout>
-    <Route exact path="/user/list" component={userList} />
-    <Route exact path="/about" component={about} />
+    <ErrorBoundary>
+      <Suspense fallback={<div>loading</div>}>
+        <Route exact path="/user/list" component={UserList} />
+        <Route exact path="/about" component={About} />
+      </Suspense>
+    </ErrorBoundary>
   </Layout>
 )
 export default routes
