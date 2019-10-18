a5b2fb848ec4b48b4f040094162ff189301b2cc3
enochjs <wangyongdong@dianwoda.com>
2019-10-18 10:42:38 +0800

    feat(master): layout add logout

---
+++ templates/react+express+typescript/project/src/view/pages/layout/header.tsx
@@ -0,0 +1,38 @@
+import React, { SFC } from 'react'
+import { Layout, Dropdown, Menu, Icon } from 'antd'
+
+const Header: SFC<{}> = function () {
+
+  function handleLogout () {
+  }
+
+  const menu = (
+    <Menu style={{ width: '200px'}}>
+      <Menu.Item style={{ borderBottom: '1px solid #f0f1f2'}}>
+        <a>上次登录时间：</a>
+        <a>IP：</a>
+        <a>城市：</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_self" rel="noopener noreferrer" onClick={handleLogout}>
+          <Icon type="logout" />退出登录
+        </a>
+      </Menu.Item>
+    </Menu>
+  )
+
+  return (
+    <Layout.Header className="layout-header">
+      <Dropdown overlay={menu} trigger={['click']}>
+        <div className="pull-right">
+          <img width="24" height="24" src="https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png" alt="avatar" />
+          <a className="ant-dropdown-link ml8" href="#">
+            enochjs
+          </a>
+        </div>
+      </Dropdown>
+    </Layout.Header>
+  )
+}
+
+export default Header
