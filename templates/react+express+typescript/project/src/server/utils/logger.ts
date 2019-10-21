03f2dbbe39cbee7df5cfd03b28f6a0e8650cfad8
enochjs <wangyongdong@dianwoda.com>
2019-10-21 10:43:54 +0800

    feat(master): logger format modify

--- templates/react+express+typescript/project/src/server/utils/logger.ts
+++ templates/react+express+typescript/project/src/server/utils/logger.ts
@@ -1,11 +1,18 @@
-import winston, { Logger, format } from 'winston'
+import winston, { Logger } from 'winston'
 import path from 'path'
 import { container } from 'inversifyExpress/index'
 
 let logger = winston.createLogger({
   transports: [
     new winston.transports.File({ filename: path.resolve(__dirname, '../../../logs/logger') }),
-  ]
+  ],
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss,SSS' }),
+    winston.format.printf(info => {
+      const { timestamp, level, message , ...data } = info 
+      return `${timestamp} ${level} ${JSON.stringify(message, null, 2)} ${JSON.stringify(data, null, 2)}`
+    })
+  )
 })
 
 if (process.env.NODE_ENV === 'localdev') {
@@ -14,8 +21,11 @@ if (process.env.NODE_ENV === 'localdev') {
       new winston.transports.Console()
     ],
     format: winston.format.combine(
-      format.timestamp(),
-      format.json()
+      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss,SSS' }),
+      winston.format.printf(info => {
+        const { timestamp, level, message , ...data } = info 
+        return `${timestamp} ${level} ${JSON.stringify(message, null, 2)} ${JSON.stringify(data, null, 2)}`
+      })
     )
   })
 }
